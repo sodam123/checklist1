f8b2fb28f25d80b01a913ba681a88c19e8bba96a
KwangHee Lim <dlarhkdgml12@naver.com>
2019-04-01 14:38:26 +0900

    Initial Commit

---
+++ checklist1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  checklist1
+//
+//  Created by Release on 01/04/2019.
+//  Copyright © 2019 GwangHee Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
